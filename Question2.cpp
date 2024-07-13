61034680d0630e6d434535e69b31f2c7010bb4f0
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2024-07-14 00:49:58 +0530

    Quetion 2 added

---
+++ Question2.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int longestSubstrWithoutRepeat(string s){
+    int ans=0;
+    map<char,int> m; 
+  
+    int j=0;
+
+    for(int i=0;i<s.length();++i){
+        char firstchar=s[i];
+        
+        if(m.find(firstchar)!=m.end() && m[firstchar]>=j){ 
+            j=m[firstchar]+1;
+        } 
+
+         m[firstchar]=i;
+         ans=max(ans,i-j+1);
+    }
+    return ans;
+}
+
+int main(){
+
+   string s;
+   cout<<"Enter the string"<<endl;
+   cin>>s;
+
+   int result=longestSubstrWithoutRepeat(s);
+
+   cout<<result<<endl;
+
+   return 0;
+}
